96ede513cc5576ef997489015ce07fa8415bd93b
George Yegon <georgeyegon67@gmail.com>
2024-09-29 16:32:25 +0300

    Added a code for array reduce method

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce((accumulator, batch) => {
+  return accumulator + batch;
+}, 0);
+
+// Optional: Log the result to verify the total
+console.log(totalBatteries); // Should print 25
